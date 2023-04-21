c63e50b6daf124cdc1b65a852ca49ca176033913
Sabrina196 <sabrinapatri96@gmail.com>
2023-04-21 12:18:07 -0300

    Ajustes en los mensajes y diseño

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ recognition.onresult= (e)=>{
 }
 
 recognition.onend= (e)=>{
-    message.innerHTML=  `<div class="alert alert-success mt-3" role="alert">
+    message.innerHTML=`<div class="alert alert-success mt-3" role="alert">
                            Fin de la Grabación
-                        </div>`  
+                        </div>` 
 }
 
 recognition.onerror= (e)=>{
@@ -30,21 +30,29 @@ recognition.onerror= (e)=>{
 }
 
 btnStart.addEventListener('click', ()=> {  
-    recognition.start();
-    message.innerHTML=  `<div class="alert alert-danger mt-3" role="alert">
+    recognition.onstart = () =>{
+    message.innerHTML=`<div class="alert alert-danger mt-3" role="alert">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-soundwave" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5zm-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5zm12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5z"/>
     </svg>
     Grabando
-    </div>`
+    </div>`}
+    recognition.start();
 });
 
 btnStop.addEventListener('click', ()=> {
     recognition.abort();
+    message.innerHTML="";
 });
 
 btnRead.addEventListener('click', ()=>{
     leerTexto(boxText.value);
+    message.innerHTML=`<div class="alert alert-warning mt-3" role="alert">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-soundwave" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5zm-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5zm12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5z"/>
+    </svg>
+    Leyendo
+    </div>`  
 });
 
 function leerTexto(boxText){
@@ -56,11 +64,22 @@ function leerTexto(boxText){
 
     window.speechSynthesis.speak(speech);
 
+    speech.onend = function(e){
+        message.innerHTML=`<div class="alert alert-success mt-3" role="alert">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-soundwave" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5zm-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zm-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5zm12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5z"/>
+    </svg>
+    Lectura Finalizada
+    </div>`
+
+    }
+    
 
 } 
 
 //Borrar Texto 
 btnDelete.addEventListener('click', ()=>{
+    message.innerHTML="";
     boxText.value="";
 })
 
